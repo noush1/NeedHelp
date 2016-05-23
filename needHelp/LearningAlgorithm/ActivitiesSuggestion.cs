0ee2f045599b99eed6909d8db1e80631a28bc741
Nuphar <nuph123@gmail.com>
2016-05-23 20:14:53 +0300

    implement MakeVector and MakeAvgVector

--- needHelp/LearningAlgorithm/ActivitiesSuggestion.cs
+++ needHelp/LearningAlgorithm/ActivitiesSuggestion.cs
@@ -55,7 +55,7 @@ namespace needHelp.LearningAlgorithm
             List<ActivityModels> AllActivities = new List<ActivityModels>();
             List<int[]> RegisteredActivitiesVectors = new List<int[]>();
             List<int[]> NotRegisteredActivitiesVec = new List<int[]>();
-            int[] avgVector = new int[_citiesAmount + _typesAmount + _organizationsAmount + _dayPartsAmount + _daysAmount];
+            double[] avgVector = new double[_citiesAmount + _typesAmount + _organizationsAmount + _dayPartsAmount + _daysAmount];
             Dictionary<ActivityModels, float> distanceDict = new Dictionary<ActivityModels, float>();
 
             RegisteredActivities = volunteer.registered_activities.ToList<ActivityModels>();
@@ -106,18 +106,174 @@ namespace needHelp.LearningAlgorithm
             return RecommendedActivities;
         }
 
+        /// <summary>
+        /// Make the vector that represent the activity or search-data
+        /// </summary>
+        /// <param name="cityId">The city id</param>
+        /// <param name="typeId">The type id</param>
+        /// <param name="orgId">The organization id</param>
+        /// <param name="date">The date</param>
+        /// <returns></returns>
         private int[] MakeVector(int? cityId, int? typeId, int? orgId, DateTime? date)
         {
-            //TODO: implement...
-            return null;
+            // Initialization
+            int[] finalVector = new int[_citiesAmount + _typesAmount + _organizationsAmount + _dayPartsAmount + _daysAmount];
+            int[] citiesVec = new int[_citiesAmount];
+            int[] typesVec = new int[_typesAmount];
+            int[] organizationsVec = new int[_organizationsAmount];
+            int[] dayPartsVec = new int[_dayPartsAmount];
+            int[] daysVec = new int[_daysAmount];
+
+            // Make cities vector
+            if (cityId != null)
+            {
+                citiesVec[cityId.Value] = 1; 
+            }
+
+            // Make types vector
+            if (typeId != null)
+            {
+                typesVec[typeId.Value] = 1;
+            }
+
+            // Make organization vector
+            if (orgId != null)
+            {
+                organizationsVec[orgId.Value] = 1;
+            }
+
+            if (date != null)
+            {
+                // Make day-parts vector
+                if (date.Value.Hour >= 6 && date.Value.Hour < 12)
+                {
+                    dayPartsVec[0] = 1;
+                }
+                else if (date.Value.Hour >= 12 && date.Value.Hour < 16)
+                {
+                    dayPartsVec[1] = 1;
+                }
+                else if (date.Value.Hour >= 16 && date.Value.Hour < 18)
+                {
+                    dayPartsVec[2] = 1;
+                }
+                else if (date.Value.Hour >= 18 && date.Value.Hour < 21)
+                {
+                    dayPartsVec[3] = 1;
+                }
+                else if ((date.Value.Hour >= 21 && date.Value.Hour <= 23) || (date.Value.Hour >= 0 && date.Value.Hour < 3))
+                {
+                    dayPartsVec[4] = 1;
+                }
+                else if (date.Value.Hour >= 3 && date.Value.Hour < 6)
+                {
+                    dayPartsVec[5] = 1;
+                }
+
+                // Make days vector
+                switch (date.Value.DayOfWeek)
+                {
+                    case DayOfWeek.Sunday:
+                        {
+                            daysVec[0] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Monday:
+                        {
+                            daysVec[1] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Tuesday:
+                        {
+                            daysVec[2] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Wednesday:
+                        {
+                            daysVec[3] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Thursday:
+                        {
+                            daysVec[4] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Friday:
+                        {
+                            daysVec[5] = 1;
+                            break;
+                        }
+                    case DayOfWeek.Saturday:
+                        {
+                            daysVec[6] = 1;
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+
+            // Build the final vector according to all the vectors that were made 
+            int i = 0;
+
+            foreach (int city in citiesVec)
+            {
+                finalVector[i] = city;
+                i++;
+            }
+
+            foreach (int type in typesVec)
+            {
+                finalVector[i] = type;
+                i++;
+            }
+
+            foreach (int organization in organizationsVec)
+            {
+                finalVector[i] = organization;
+                i++;
+            }
+
+            foreach (int dayPart in dayPartsVec)
+            {
+                finalVector[i] = dayPart;
+                i++;
+            }
+
+            foreach (int day in daysVec)
+            {
+                finalVector[i] = day;
+                i++;
+            }
+
+            return finalVector;
         }
 
-        private void MakeAvgVector(List<int[]> vectors, int[] avgVector)
+        /// <summary>
+        /// Make the average vector of all the activities vectors
+        /// </summary>
+        /// <param name="vectors"></param>
+        /// <param name="avgVector"></param>
+        private void MakeAvgVector(List<int[]> vectors, double[] avgVector)
         {
-            //TODO: implement...
+            int vectorsAmount = vectors.Count();
+            int vectorSize = _citiesAmount + _typesAmount + _organizationsAmount + _dayPartsAmount + _daysAmount;
+
+            // Sum up the values for the i cell in all the vector and divide in the number of vectors
+            for (int i = 0; i < vectorSize; i++)
+            {
+                int sumOfCurrCell = 0;
+
+                for (int j = 0; j < vectorsAmount; j++)
+                {
+                    sumOfCurrCell += vectors.ElementAt(j)[i];
+                }
+
+                avgVector[i] = (double)(sumOfCurrCell / vectorsAmount);
+            }
         }
 
-        private void CalculateDistance(Dictionary<ActivityModels, float> dict, int[] avgVector, List<int[]> activitiesVect)
+        private void CalculateDistance(Dictionary<ActivityModels, float> dict, double[] avgVector, List<int[]> activitiesVect)
         {
             //TODO: implement...
         }
